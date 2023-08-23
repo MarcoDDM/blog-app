dfb81379c9b9ca70d7846a761cf9a2965268d760
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-08-23 16:31:33 -0300

    Migration

--- db/migrate/20230822155842_create_comments.rb
+++ db/migrate/20230822155842_create_comments.rb
@@ -7,7 +7,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :comments, :user_id
-    add_index :comments, :post_id
+   # add_index :comments, :user_id
+   # add_index :comments, :post_id
   end
 end
